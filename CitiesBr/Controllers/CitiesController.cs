b2c031338ac6d2077cd9b9f13ec0d1e603b62816
Igor Couto <igor.fcouto@gmail.com>
2020-05-16 16:43:42 -0300

    Initial Commit

---
+++ CitiesBr/Controllers/CitiesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CitiesBr.Model;
+
+namespace CitiesBr.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CitiesController : ControllerBase
+    {
+        private readonly ILogger<CitiesController> _logger;
+
+        public CitiesController(ILogger<CitiesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<City> Get()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CitiesBr/Model/City.cs
@@ -0,0 +1,9 @@
+﻿namespace CitiesBr.Model
+{
+    public class City
+    {
+        internal string Name { get; set; }
+        internal int Population { get; set; }
+        internal int State { get; set; }
+    }
+}
