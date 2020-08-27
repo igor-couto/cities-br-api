501d32721eea9d4ce06dd1bce08f9907a9bb5494
Igor Couto <igor.fcouto@gmail.com>
2020-08-27 00:06:10 -0300

    Add nlog and swagger configuration

--- CitiesBr/Program.cs
+++ CitiesBr/Program.cs
@@ -1,11 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NLog.Web;
 
 namespace CitiesBr
 {
@@ -21,6 +17,10 @@ namespace CitiesBr
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                }).UseNLog();
     }
 }
--- CitiesBr/Startup.cs
+++ CitiesBr/Startup.cs
@@ -1,33 +1,53 @@
+using System;
+using System.IO;
+using System.Reflection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 
 namespace CitiesBr
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            
+            AddSwagger(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage();
-            }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(options => {
+
+                options.DocumentTitle = "API Cidades Br";
+
+                //options.SwaggerEndpoint("../swagger/v1/swagger.json", "v1");
+
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+
+                options.RoutePrefix = string.Empty;
+
+                options.InjectStylesheet("../custom.css");
+
+            });
 
             app.UseHttpsRedirection();
 
@@ -37,6 +57,40 @@ namespace CitiesBr
 
             app.UseEndpoints(endpoints => {
                 endpoints.MapControllers();
+            });
+        }
+
+        private void AddSwagger(IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                var contato = new OpenApiContact() 
+                { 
+                    Name = "Igor Freitas Couto",
+                    Email = "igor.fcouto@gmail.com",
+                    Url = new Uri("https://github.com/igor-couto/cities-br-api") 
+                };
+
+                var licenca = new OpenApiLicense()
+                {
+                   Name = "Licença Apache 2.0",
+                   Url = new Uri("https://www.apache.org/licenses/LICENSE-2.0.html")
+                };
+
+                options.SwaggerDoc("v1", 
+                    new OpenApiInfo { 
+                        Title = "API Cidades Br", 
+                        Version = "1.0", 
+                        Description = "API de Cidades do Brasil", 
+                        Contact = contato,
+                        License = licenca
+                    });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+
+                if (File.Exists(xmlPath))
+                    options.IncludeXmlComments(xmlPath);
             });
         }
     }
